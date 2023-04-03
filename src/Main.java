465dc82ec8a336b792bd4871f914672648a07a23
dalden <daldenuteryag@gmail.com>
2023-04-03 12:56:51 +0600

    finding the factorial of the number

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of elements in array: ");
+        System.out.print("Enter the number: ");
         int num = sc.nextInt();
-        int[] arr = new int[num];
-        System.out.print("Enter the numbers: ");
-        for (int i = 0; i < num; i++){
-            arr[i] = sc.nextInt();
-        }
-        double sum = findAverage(arr, num);
-        System.out.println(sum / num);
+        System.out.println("The factorial of the given number is: " + factorial(num));
     }
 
     public static int findMin(int[] arr, int n) {
@@ -31,6 +25,14 @@ public class Main {
         } else {
             double sum = findAverage(arr, n-1);
             return sum + arr[n-1];
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
         }
     }
 
